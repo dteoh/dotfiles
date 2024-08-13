6881ac333141fa4cc156aa9d43cbdca8e076b18e
Douglas Teoh <douglas@dteoh.com>
2024-08-13 20:33:31 +1000

    Add wezterm config (#5)

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,46 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
+config.font = wezterm.font 'Hack'
+config.font_size = 16.0
+
+config.color_scheme = 'Sonokai (Gogh)'
+
+config.keys = {
+    {
+        key = '\r',
+        mods = 'SUPER',
+        action = wezterm.action.SplitPane {
+            direction = 'Down'
+        }
+    },
+    {
+        key = '\r',
+        mods = 'SUPER|SHIFT',
+        action = wezterm.action.SplitPane {
+            direction = 'Right'
+        }
+    },
+    {
+        key = 'l',
+        mods = 'SUPER',
+        action = wezterm.action.RotatePanes 'Clockwise'
+    },
+    {
+        key = 'l',
+        mods = 'SUPER|SHIFT',
+        action = wezterm.action.RotatePanes 'CounterClockwise'
+    }
+}
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+return config
